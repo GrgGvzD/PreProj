6b1a90e1a26ad6e31c062e873640a761c9198376
EgorLum <grggvzd@gmail.com>
2023-06-07 13:12:09 +0300

    ничего не работает, подозреваю что проблемма в классе Util, соединение == null, не разберусь в чем дело.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,6 +8,19 @@ public class Main {
     private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
        // Util util = new Util();
+        userService.createUsersTable();
 
+        userService.saveUser("Джо", "Байден", (byte) 78);
+        userService.saveUser("Трамп", "Дональд", (byte) 74);
+        userService.saveUser("Барак", "Обама", (byte) 59);
+        userService.saveUser("Джордж", "Буш", (byte) 74);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
